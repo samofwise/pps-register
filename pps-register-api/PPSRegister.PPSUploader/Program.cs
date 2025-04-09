286edeb7da710d17796efcd18f7740c5379f5d63
samofwise <sam.ed.henry@gmail.com>
2025-04-10 09:21:42 +1000

    Quickly fix the credentials issue

--- pps-register-api/PPSRegister.PPSUploader/Program.cs
+++ pps-register-api/PPSRegister.PPSUploader/Program.cs
@@ -1,5 +1,6 @@
 using Amazon;
 using Amazon.Extensions.NETCore.Setup;
+using Amazon.Runtime;
 using Amazon.SQS;
 using Microsoft.EntityFrameworkCore;
 using PPSRegister.Data;
@@ -8,10 +9,15 @@ using PPSRegister.PPSUploader.Workers;
 
 var builder = Host.CreateApplicationBuilder(args);
 
+// Move these to environment variables
 builder.Services.AddAWSService<IAmazonSQS>(new AWSOptions
 {
-  Profile = "development-user",
-  Region = RegionEndpoint.APSoutheast2
+  Region = RegionEndpoint.APSoutheast2,
+  Credentials = new SessionAWSCredentials(
+        "ASIA5FHKA3ARWXBQFYTK",
+        "fcOPRrxagvtQOylGfWTycTQmfTvPCQ19H/VU5Pcf",
+        "IQoJb3JpZ2luX2VjEB8aDmFwLXNvdXRoZWFzdC0yIkgwRgIhAM1tgqSaDSi4tX9PV+MBKccwWMsTrJ7uwM3d9B4k3Rs7AiEA9vH2oGESctN+f0Mqs9dyV/f+Wxt7WzZ/zg0zCmIWrmkqhgMImP//////////ARADGgw5MDQ1ODE0MDQ3MDciDJ0OBBts5C3u2XiaFiraAuIAAtzRBU494/JKHwYQhwRVOzk9ZI9tpBFjBxiZsO5oHPobNku7dyqixuqOJ697tcueAqERtOtyl8kdyksl5Itb10Bws1uJkULRQSwAB8B+mjounRRkMmWC0hlUssOnscqIh5SS6v2bzHZHMWJupNm8aO7k3lLNzPrvoh92OyFIFCpLU1ashxYuiiM6QTLb+DK/4mdhyQ6a6j7IGrJ/Ta/sW/qMn7rptFcYSb5iZVjqfufDN9bK7FJJGdVNH1qPDvE0v6aZe8YnNb75S+0xFfJR0WQ1WhH/Rf2JO7pDdxBczNmejk7NJx8e1yXyn1Jw8dpiDsaTbr7YsWfzgrdmZl6zLalEuNJw8yQB0Q2gpkrulSdWrjTtRcMi97Uhb4As4s9oRHGhxpVgXLwLbEA7X12fwGuYLecWih59Hea9wxhXHZrXnaAtVhJpsfDc8kI5ffjmdOmzFlX1UKEw9P/bvwY6pgElSRCr5/AsY+/z3JSeI2AAblf5JPPAaKLAm/9MZU/BcBsv8eJDgmtBX81NKWLskCakEq+dSNhNzN2eXIpVt2NRhV7Dd60RfAuzibXZQleLJERFH+C64fRnQPRlL0v3oyJsf3ROFEPJdmD6MY+naMlgJX1ZnRaboczMCFSUWSq38QgXZ5HSYAyGrmKkinfOouUwL4FADBrodLqiYLyEeri3vkrBYDfE"
+    )
 });
 
 builder.Services.AddDbContextFactory<PPSRegisterDbContext>(options =>
