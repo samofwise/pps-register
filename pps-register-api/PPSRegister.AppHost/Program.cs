49bab60fd13412fc09c542d7117ba7b4ee3594b9
samofwise <sam.ed.henry@gmail.com>
2025-04-10 12:57:20 +1000

    Create docker files for each app

--- pps-register-api/PPSRegister.AppHost/Program.cs
+++ pps-register-api/PPSRegister.AppHost/Program.cs
@@ -7,35 +7,32 @@ var awsRegion = "ap-southeast-2";
 var queueUrl = "https://sqs.ap-southeast-2.amazonaws.com/904581404707/PPSUploaderQueue";
 
 var sql = builder.AddSqlServer("sql")
-                 .WithLifetime(ContainerLifetime.Persistent);
+  .WithLifetime(ContainerLifetime.Persistent)
+  .WithContainerName("aspire-pps-register-sql");
 
 var db = sql.AddDatabase("PPSRegister");
 
-
-var uploader = builder.AddProject<Projects.PPSRegister_PPSUploader>("ppsuploader")
+var uploader = builder.AddProject<Projects.PPSRegister_PPSUploader>("aspire-pps-register-uploader")
     .WithReference(db)
     .WaitFor(db)
     .WithEnvironment("AWS__Region", awsRegion)
-    .WithEnvironment("AWS__SQS__QueueUrl", queueUrl)
-    .PublishAsDockerFile();
+    .WithEnvironment("AWS__SQS__QueueUrl", queueUrl);
 
-var api = builder.AddProject<Projects.PPSRegister_Api>("api")
+var api = builder.AddProject<Projects.PPSRegister_Api>("aspire-pps-register-api")
     .WithReference(db)
     .WaitFor(db)
     .WithReference(uploader)
     .WithHttpsEndpoint(name: "api", port: 4001)
     .WithEnvironment("AWS__Region", awsRegion)
-    .WithEnvironment("AWS__SQS__QueueUrl", queueUrl)
-    .PublishAsDockerFile();
+    .WithEnvironment("AWS__SQS__QueueUrl", queueUrl);
 
-builder.AddNpmApp("vite", "../../pps-register-app", "start")
+builder.AddNpmApp("aspire-pps-register-app", "../../pps-register-app", "start")
   .WithReference(api)
   .WaitFor(api)
   .WithEnvironment("BROWSER", "none")
   .WithEnvironment("VITE_API_URL", api.GetEndpoint("api"))
   .WithHttpEndpoint(port: 3500, env: "VITE_PORT")
   .WithExternalHttpEndpoints()
-  .PublishAsDockerFile()
   .WithEnvironment("NODE_TLS_REJECT_UNAUTHORIZED", "0");
 
 builder.Build().Run();
